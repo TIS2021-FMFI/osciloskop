537b270e96f2abbd533a919aee29d5fa6c526652
YelovSK <hampatrik@gmail.com>
2021-11-12 02:58:06 +0100

    connected to adapter

--- src/backend/functions.py
+++ src/backend/functions.py
@@ -1,5 +1,22 @@
-def connect():
-    print("connect")
+import backend.adapter as ada
 
-def disconnect():
-    print("disconnect")
+class Functions:
+
+    def __init__(self):
+        self.adapter = ada.Adapter(testing=True)
+        self.adapter.start_hpctrl()
+
+    def connect(self, address: int = 7):
+        self.adapter.connect(address)
+
+    def disconnect(self):
+        self.adapter.disconnect()
+
+    def enter_cmd(self):
+        self.adapter.enter_cmd_mode()
+
+    def exit_cmd(self):
+        self.adapter.exit_cmd_mode()
+
+    def send_custom(self, message: str):
+        self.adapter.send(message.split("\n"))
