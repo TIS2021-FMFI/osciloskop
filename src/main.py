953da8b6ffc6112d78e1c3e08e0dad87be179032
YelovSK <hampatrik@gmail.com>
2021-11-12 02:57:52 +0100

    now launches GUI

--- src/main.py
+++ src/main.py
@@ -1,14 +1,5 @@
 import frontend.gui as fr
-import backend.adapter as ada
 
 if __name__ == "__main__":
-    a = ada.Adapter(True)
-
-    a.start_hpctrl()
-    a.connect(7)
-    a.enter_cmd_mode()
-    a.send(["q aaaaa"])
-    a.exit_cmd_mode()
-    a.disconnect()
-
-    # fr.run()
+    gui = fr.GUI()
+    gui.run()
