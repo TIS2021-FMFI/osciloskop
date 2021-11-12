236a2cee6017a9473cfe4e93a4dd0cad13e8a9a9
YelovSK <hampatrik@gmail.com>
2021-11-12 02:57:38 +0100

    put into a class

--- src/frontend/gui.py
+++ src/frontend/gui.py
@@ -1,46 +1,55 @@
 import PySimpleGUI as sg
-from backend import functions as f
+import backend.functions as f
 
-sg.theme("DarkAmber")
+class GUI:
 
-button_size = (10, 1)
-# Elements inside the window
-win_1 = [
-    [sg.InputText("Address No.", key="terminal", size=(12, 1)), sg.Button("Connect", size=button_size)],
-    [sg.Button("Terminal", size=button_size), sg.Button("Disconnect", size=button_size)]
-]
-win_2 = [
-    [sg.InputText("Average No.", size=(15, 1), key="average")],
-    [sg.InputText("Points 16-4096", size=(15, 1), key="points")],
-    [sg.Button("RESET", size=(12, 1))]
-]
-win_3 = [
-    [sg.Checkbox("Channel 1"), sg.Checkbox("Channel 2")],
-    [sg.Checkbox("Channel 3"), sg.Checkbox("Channel 4")],
-    [sg.Combo(values=["RAW", "average", "histogram H/V", "versus"], default_value="RAW")],
-    [sg.InputText("FILE_NAME", size=(25, 1)), sg.SaveAs("PATH/")],
-    [sg.Button("SAVE", size=button_size), sg.Checkbox("AutoSave")],
-    [sg.Button("SINGLE", size=button_size), sg.Button("STOP", size=button_size), sg.Button("SINGLE", size=button_size)]
-]
+    def __init__(self):
+        self.func = f.Functions()
+        sg.theme("DarkAmber")
+        self.layout = self.create_layout()
+        self.window = sg.Window("GUI application", self.layout, size=(500, 500), element_justification="center")
 
-layout = [
-    [sg.Frame(layout=win_1, title='', element_justification="c")],
-    [sg.Frame(layout=win_2, title='', element_justification="c")],
-    [sg.Frame(layout=win_3, title='', element_justification="c")]
-]
+    def create_layout(self): 
+        button_size = (10, 1)
+        # Elements inside the window
+        win_1 = [
+            [sg.InputText("Address No.", key="terminal", size=(12, 1)), sg.Button("Connect", size=button_size)],
+            [sg.Button("Terminal", size=button_size), sg.Button("Disconnect", size=button_size)]
+        ]
+        win_2 = [
+            [sg.InputText("Average No.", size=(15, 1), key="average")],
+            [sg.InputText("Points 16-4096", size=(15, 1), key="points")],
+            [sg.Button("RESET", size=(12, 1))]
+        ]
+        win_3 = [
+            [sg.Checkbox("Channel 1"), sg.Checkbox("Channel 2")],
+            [sg.Checkbox("Channel 3"), sg.Checkbox("Channel 4")],
+            [sg.Combo(values=["RAW", "average", "histogram H/V", "versus"], default_value="RAW")],
+            [sg.InputText("FILE_NAME", size=(25, 1)), sg.SaveAs("PATH/")],
+            [sg.Button("SAVE", size=button_size), sg.Checkbox("AutoSave")],
+            [sg.Button("SINGLE", size=button_size), sg.Button("STOP", size=button_size), sg.Button("SINGLE", size=button_size)]
+        ]
+        win_4 = [
+            [sg.Button("Send custom", size=button_size), sg.InputText("AAA", size=(15, 1), key="custom")],
+            [sg.Button("Quit GUI", size=button_size)]
+        ]
 
-# Create the window
-window = sg.Window("GUI application", layout, size=(500, 400), element_justification="center")
+        return [
+            [sg.Frame(layout=win, title='', element_justification="c")]
+            for win in (win_1, win_2, win_3, win_4)
+        ]
 
-def run():
-    # Event loop
-    while True:
-        event, values = window.read()
-        if event == "Connect":
-            f.connect()
-        if event == "Disconnect":
-            f.disconnect()
-        if event == sg.WIN_CLOSED:
-            break
+    def run(self):
+        # Event loop
+        while True:
+            event, values = self.window.read()
+            if event == "Connect":
+                self.func.connect()
+            if event == "Disconnect":
+                self.func.disconnect()
+            if event == "Send custom":
+                self.func.send_custom(values["custom"])
+            if event in (sg.WIN_CLOSED, "Quit GUI"):
+                break
 
-    window.close()
+        self.window.close()
