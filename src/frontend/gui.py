b6b5333d7d0eefba1938e2c432d06e7b40f7cc52
YelovSK <hampatrik@gmail.com>
2021-12-16 15:58:50 +0100

    refactor

--- src/frontend/gui.py
+++ src/frontend/gui.py
@@ -1,27 +1,8 @@
 import os
+import re
 import PySimpleGUI as sg
 from backend.adapter import AdapterError
-from backend.command import (
-    AverageCmd,
-    AverageNoCmd,
-    CheckIfResponsiveCmd,
-    CommandError,
-    CustomCmd,
-    CustomCmdWithOutput,
-    DisconnectCmd,
-    ExitHpctrlCmd,
-    FactoryResetCmd,
-    InitializeCmds,
-    LeaveCmdModeCmd,
-    PointsCmd,
-    PreambleOffCmd,
-    PreampleOnCmd,
-    SingleCmds,
-    StartRunCmds,
-    StopRunCmds,
-)
-from PySimpleGUI.PySimpleGUI import popup_yes_no
-
+from backend.command import *
 
 class GUI:
 
@@ -53,14 +34,15 @@ class GUI:
 
     def __init__(self):
         sg.theme("DarkGrey9")
-        self.currently_set_values = {self.channels: []}
         self.layout = self._create_layout()
         self.window = sg.Window(
             "Oscilloscope control",
             self.layout,
             size=(self.WIDTH, self.HEIGHT),
             element_justification="c",
+            finalize=True
         )
+        self.initialize_set_values()
 
     def _create_layout(self):
         button_size = (10, 1)
@@ -130,7 +112,7 @@ class GUI:
                         key=self.curr_path, default_text="assets/measurements", enable_events=True
                     )
                 ],
-                [sg.FolderBrowse("Browse")],
+                [sg.FolderBrowse("Browse", initial_folder="assets", change_submits=True, enable_events=True)],
                 [
                     sg.Button(self.run, size=button_size, disabled=True),
                     sg.Button(self.single, size=button_size, disabled=True),
@@ -147,8 +129,7 @@ class GUI:
                     sg.Button(self.load_config),
                     sg.Combo(
                         values=[
-                            f
-                            for f in os.listdir(os.path.join("assets", "config"))
+                            f for f in os.listdir(os.path.join("assets", "config"))
                             if f.endswith(".txt")
                         ],
                         key=self.config_file,
@@ -171,6 +152,14 @@ class GUI:
             [sg.Frame("Oscilloscope settings", [[col_osci]]), sg.Frame("Config", [[col_cfg]])],
             [sg.Frame("Info", [[col_info]])],
         ]
+        
+    def initialize_set_values(self):
+        self.currently_set_values = {self.channels: []}
+        self.currently_set_values[self.average_pts] = AverageNoCmd().get_set_value()
+        self.currently_set_values[self.curr_points] = PointsCmd().get_set_value()
+        self.window[self.curr_points].update(self.currently_set_values[self.curr_points])
+        self.window[self.curr_average_no].update(self.currently_set_values[self.average_pts])
+        self.update_info()
 
     def open_config_creation(self):
         # opens a new window for creating a new configuration file
@@ -247,8 +236,7 @@ class GUI:
 
     def create_config_file(self, config_content, file_name):
         if config_content:
-            with open(os.path.join("assets", "config", f"{file_name}.txt"), "w") as f:
-                f.write(config_content)
+            open(os.path.join("assets", "config", f"{file_name}.txt"), "w").write(config_content)
         self.window[self.config_file].update(
             values=[f for f in os.listdir(os.path.join("assets", "config")) if f.endswith(".txt")]
         )
@@ -264,19 +252,20 @@ class GUI:
             self.window[i].update(disabled=disable)
 
     def open_terminal_window(self):
+        cmd_input, cmd_output, cmd_send = "cin", "cout", "csend"
         layout = [
-            [sg.InputText(key="cmd_input"), sg.Button("cmd_send", bind_return_key=True)],
-            [sg.Multiline(key="cmd_output", disabled=True, size=(450, 450), autoscroll=True)],
+            [sg.InputText(key=cmd_input, size=(50, 20)), sg.Button("Send", key=cmd_send, bind_return_key=True)],
+            [sg.Multiline(key=cmd_output, disabled=True, size=(60, 450), autoscroll=True)],
         ]
-        window = sg.Window("Terminal", layout, size=(500, 500))
+        window = sg.Window("Terminal", layout, size=(450, 500))
         while True:
             event, values = window.read()
-            if event == "cmd_send":
-                window["cmd_input"].update("")
-                cmd_in = values["cmd_input"]
-                window["cmd_output"].update(value=f">>> {cmd_in}\n", append=True)
+            if event == cmd_send:
+                window[cmd_input].update("")
+                cmd_in = values[cmd_input]
+                window[cmd_output].update(value=f">>> {cmd_in}\n", append=True)
                 if cmd_in in ("clr", "cls", "clear"):
-                    window["cmd_output"].update("")
+                    window[cmd_output].update("")
                     continue
                 if len(cmd_in.split()) > 1 and cmd_in.split()[0] == "q":  # asking for output
                     try:
@@ -284,116 +273,131 @@ class GUI:
                     except AdapterError as e:
                         sg.popup(e)
                         continue
-                    window["cmd_output"].update(value=output + "\n", append=True)
+                    window[cmd_output].update(value=output + "\n", append=True)
                 else:
                     CustomCmd(cmd_in).do()
             elif event in (sg.WIN_CLOSED, "Close"):
                 break
         window.close()
+        
+    def get_mismatched_inputboxes(self, values):
+        res = []
+        if values[self.curr_average_no] != self.currently_set_values[self.average_pts]:
+            res.append("Average No.")
+        if values[self.curr_points] != self.currently_set_values[self.curr_points]:
+            res.append("Points")
+        return res
+
+    def event_check(self) -> bool:  # returns False if closed
+        event, values = self.window.read()
+        if event == self.connect:
+            InitializeCmds(values[self.address]).do()
+            self.currently_set_values[self.address] = values[self.address]
+            self.button_activation(False)
+
+        elif event == self.disconnect:
+            LeaveCmdModeCmd().do()
+            DisconnectCmd().do()
+            self.button_activation(True)
+            self.currently_set_values[self.address] = 0
+
+        elif event == self.new_config:
+            config_content, config_name = self.open_config_creation()
+            self.create_config_file(config_content, config_name)
+
+        elif event == self.load_config:
+            file_name = values[self.config_file]
+            full_path = os.path.join("assets", "config", file_name)
+            if not os.path.isfile(full_path):
+                sg.popup("File does not exist")
+                return True
+            if file_name:
+                self.open_config_window(full_path)
+            else:
+                sg.popup("File not chosen")
+
+        elif event == self.set_points:
+            PointsCmd(values[self.curr_points]).check_and_do()
+            self.currently_set_values["points"] = values[self.curr_points]
+
+        elif event == self.set_average_no:
+            AverageNoCmd(values[self.curr_average_no]).check_and_do()
+            self.currently_set_values[self.average_pts] = values[self.curr_average_no]
+
+        elif event in (self.ch1, self.ch2, self.ch3, self.ch4):
+            if values[event]:
+                self.currently_set_values[self.channels].append(event)
+            else:
+                self.currently_set_values[self.channels].remove(event)
+
+        elif event == self.averaging:
+            AverageCmd([values[self.averaging]]).do()
+            self.currently_set_values["average"] = values[self.averaging]
+
+        elif event == self.reinterpret_trimmed_data:
+            ...
+
+        elif event == self.set_preamble:
+            if values[self.set_preamble] == True:
+                PreampleOnCmd().do()
+            else:
+                PreambleOffCmd().do()
+            self.currently_set_values["preamble"] = values[self.set_preamble]
+
+        elif event == self.single:
+            mismatched = self.get_mismatched_inputboxes(values)
+            if mismatched:
+                sg.popup(f"Values are not set: {', '.join(mismatched)}")
+                return True
+            channels = self.currently_set_values[self.channels]
+            path = values[self.curr_path].replace("/", os.sep)
+            if channels:
+                SingleCmds(channels, path).do()
+            else:
+                sg.popup("No channels were selected")
+
+        elif event == self.run:
+            mismatched = self.get_mismatched_inputboxes(values)
+            if mismatched:
+                sg.popup(f"Values are not set: {', '.join(mismatched)}")
+                return True
+            channels = self.currently_set_values[self.channels]
+            if not channels:
+                sg.popup("No channels were selected")
+                return True
+            dir_path = values[self.curr_path]
+            is_premble = self.currently_set_values["preamble"]
+            temp_file = "assets/measurements/temp.txt"
+            StartRunCmds(temp_file, channels).do()
+            if sg.popup(custom_text="stop", title="Running", keep_on_top=True) == "stop":
+                StopRunCmds(temp_file, values[self.curr_path], channels, is_premble).do()
+
+        elif event == self.factory_reset_osci:
+            if sg.popup_yes_no(title="Reset?", keep_on_top=True) == "Yes":
+                FactoryResetCmd().do()
+
+        elif event == self.ping_osci:
+            msg = "ping successful" if CheckIfResponsiveCmd().do() else "couldn't ping"
+            sg.popup(msg)
+
+        elif event == self.terminal:
+            self.open_terminal_window()
+            
+        elif event == "Browse":
+            self.window[self.curr_path].update(values["Browse"])
+
+        elif event in (sg.WIN_CLOSED, self.quit_gui):
+            return False
+
+        self.update_info()
+
+        return True
 
     def main_loop(self):
-        # Event loop
         while True:
-            event, values = self.window.read()
             try:
-                if event == self.connect:
-                    InitializeCmds(values[self.address]).do()
-                    self.currently_set_values[self.address] = values[self.address]
-                    self.button_activation(False)
-
-                elif event == self.disconnect:
-                    LeaveCmdModeCmd().do()
-                    DisconnectCmd().do()
-                    self.button_activation(True)
-                    self.currently_set_values[self.address] = 0
-
-                elif event == self.new_config:
-                    config_content, config_name = self.open_config_creation()
-                    self.create_config_file(config_content, config_name)
-
-                elif event == self.load_config:
-                    file_name = values[self.config_file]
-                    full_path = os.path.join("assets", "config", file_name)
-                    if not os.path.isfile(full_path):
-                        sg.popup("File does not exist")
-                        continue
-                    if file_name:
-                        self.open_config_window(full_path)
-                    else:
-                        sg.popup("File not chosen")
-
-                elif event == self.set_points:
-                    PointsCmd(values[self.curr_points]).check_and_do()
-                    self.currently_set_values["points"] = values[self.curr_points]
-
-                elif event == self.set_average_no:
-                    AverageNoCmd(values[self.curr_average_no]).check_and_do()
-                    self.currently_set_values[self.average_pts] = values[self.curr_average_no]
-
-                elif event in (self.ch1, self.ch2, self.ch3, self.ch4):
-                    if values[event]:
-                        self.currently_set_values[self.channels].append(event)
-                    else:
-                        self.currently_set_values[self.channels].remove(event)
-
-                elif event == self.averaging:
-                    if values[event] == True:
-                        AverageCmd(True).do()
-                    else:
-                        AverageCmd(False).do()
-                    self.currently_set_values["average"] = values[event]
-
-                elif event == self.reinterpret_trimmed_data:
-                    ...
-
-                elif event == self.set_preamble:
-                    if values[event] == True:
-                        PreampleOnCmd().do()
-                    else:
-                        PreambleOffCmd().do()
-                    self.currently_set_values["preamble"] = values[event]
-
-                elif event == self.single:
-                    channels = self.currently_set_values[self.channels]
-                    if not os.path.isdir(values[self.curr_path]):
-                        sg.popup("Path does not exist")
-                        continue
-                    if channels:
-                        SingleCmds(channels, values[self.curr_path]).do()
-                    else:
-                        sg.popup("No channels were selected")
-
-                elif event == self.run:
-                    channels = self.currently_set_values[self.channels]
-                    if not channels:
-                        sg.popup("No channels were selected")
-                        continue
-                    dir_to_store_measurements = values[self.curr_path]
-                    if not os.path.isdir(dir_to_store_measurements):
-                        sg.popup("Path does not exist")
-                        continue
-                    isPreamble = self.currently_set_values["preamble"]
-                    tempfile = "assets/measurements/temp.txt"
-                    StartRunCmds(tempfile, channels).do()
-                    if sg.popup(custom_text="stop", title="Running", keep_on_top=True) == "stop":
-                        StopRunCmds(tempfile, values[self.curr_path], channels, isPreamble).do()
-
-                elif event == self.factory_reset_osci:
-                    if popup_yes_no(title="Reset?", keep_on_top=True) == "Yes":
-                        FactoryResetCmd().do()
-
-                elif event == self.ping_osci:
-                    msg = "ping successful" if CheckIfResponsiveCmd().do() else "couldn't ping"
-                    sg.popup(msg)
-
-                elif event == self.terminal:
-                    self.open_terminal_window()
-
-                elif event in (sg.WIN_CLOSED, self.quit_gui):
+                if not self.event_check():
                     break
-
-                self.update_info()
-
             except (CommandError, AdapterError) as e:
                 sg.popup(e)
 
